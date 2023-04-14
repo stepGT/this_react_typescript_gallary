ee8d5889fef028e08b176cdcaf7eadbd1af5cea2
Andrey Zuev <stepgt@mail.ru>
2023-04-14 14:22:00 +1000

    Styled MainPhoto component

--- src/components/MainPhoto/index.tsx
+++ src/components/MainPhoto/index.tsx
@@ -1,13 +1,33 @@
+import cn from 'classnames';
 import { ICommon, IPhoto } from '../types';
+import style from './index.module.scss';
 
 interface IMainPhoto extends ICommon {
-  prevPhoto: IPhoto;
+  prevPhoto?: IPhoto;
   activePhoto: IPhoto;
-  nextPhoto: IPhoto;
+  nextPhoto?: IPhoto;
 }
 
-const MainPhoto: React.FC<IMainPhoto> = ({ prevPhoto, activePhoto, nextPhoto }) => {
-  return <div className="MainPhoto">MainPhoto</div>;
+const MainPhoto: React.FC<IMainPhoto> = ({ prevPhoto, activePhoto, nextPhoto, className }) => {
+  return (
+    <div className={cn(className, style.mainPhoto)}>
+      {prevPhoto && (
+        <img
+          className={style.mainPhotoImagePrev}
+          src={prevPhoto.src}
+          alt={prevPhoto.description}
+        />
+      )}
+      <img className={style.mainPhotoImage} src={activePhoto.src} alt={activePhoto.description} />
+      {nextPhoto && (
+        <img
+          className={style.mainPhotoImageNext}
+          src={nextPhoto.src}
+          alt={nextPhoto.description}
+        />
+      )}
+    </div>
+  );
 };
 
 export default MainPhoto;
