ebe3831be5fdbbc294c037e69fc0a944310caaff
Andrey Zuev <stepgt@mail.ru>
2023-04-17 14:08:32 +1000

    Styled PreviewGallery component

--- src/components/PreviewGallery/index.tsx
+++ src/components/PreviewGallery/index.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect, useMemo } from 'react';
 import cn from 'classnames';
 import { IPhoto, ICommon } from '../types';
 import style from './index.module.scss';
@@ -9,21 +10,31 @@ interface IPreviewGallery extends ICommon {
 }
 
 const PreviewGallery: React.FC<IPreviewGallery> = ({ photos, activePhotoIndex, className }) => {
+  const previewContainer = useRef<HTMLUListElement>(null);
+  useEffect(() => {
+    if (!previewContainer.current) return;
+    previewContainer.current.style.transform = `translate3d(-${activePhotoIndex * 164}px, 0, 0)`;
+  }, [activePhotoIndex]);
   return (
     <div className={cn(style.previewGallery, className)}>
-      <ul className={style.previewGalleryTrack}>
-        {photos.map(photo => (
-          <li key={photo.id}>
-            <button className={style.previewGalleryPreview} onClick={() => {}}>
-              <img
-                className={style.previewGalleryImage}
-                src={photo.preview}
-                alt={photo.description}
-              />
-            </button>
-          </li>
-        ))}
-      </ul>
+      {useMemo(
+        () => (
+          <ul className={style.previewGalleryTrack} ref={previewContainer}>
+            {photos.map((photo) => (
+              <li key={photo.id}>
+                <button className={style.previewGalleryPreview} onClick={() => {}}>
+                  <img
+                    className={style.previewGalleryImage}
+                    src={photo.preview}
+                    alt={photo.description}
+                  />
+                </button>
+              </li>
+            ))}
+          </ul>
+        ),
+        [],
+      )}
       <div className={style.previewGalleryCover}>
         {activePhotoIndex + 1} / {photos.length}
       </div>
