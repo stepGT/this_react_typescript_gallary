39c8516031da7ecc86089ac00ca9ba8e4ce0cf4e
Andrey Zuev <stepgt@mail.ru>
2023-04-18 11:15:54 +1000

    Added transition photo

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -12,19 +12,13 @@ interface IGallery {
 const Gallery: React.FC<IGallery> = ({ photos }) => {
   if (!photos.length) return null;
   const [indexActivePhoto, setIndexActivePhoto] = useState(0);
-  const activePhoto = photos[indexActivePhoto];
   const prevPhoto = photos[indexActivePhoto - 1];
   const nextPhoto = photos[indexActivePhoto + 1];
   //
   return (
     <div className={style.gallery}>
       <div className={style.galleryContainer}>
-        <MainPhoto
-          prevPhoto={prevPhoto}
-          activePhoto={activePhoto}
-          nextPhoto={nextPhoto}
-          className={style.mainPhoto}
-        />
+        <MainPhoto photos={photos} indexActivePhoto={indexActivePhoto} />
         <Navigation
           className={style.galleryNavigation}
           disabledNext={!nextPhoto}
--- src/components/MainPhoto/index.tsx
+++ src/components/MainPhoto/index.tsx
@@ -1,32 +1,91 @@
+import { useRef, useMemo, useState, useLayoutEffect } from 'react';
 import cn from 'classnames';
 import { ICommon, IPhoto } from '../types';
 import style from './index.module.scss';
 
 interface IMainPhoto extends ICommon {
-  prevPhoto?: IPhoto;
-  activePhoto: IPhoto;
-  nextPhoto?: IPhoto;
+  photos: IPhoto[];
+  indexActivePhoto: number;
 }
 
-const MainPhoto: React.FC<IMainPhoto> = ({ prevPhoto, activePhoto, nextPhoto, className }) => {
-  return (
-    <div className={cn(className, style.mainPhoto)}>
-      {prevPhoto && (
-        <img
-          className={style.mainPhotoImagePrev}
-          src={prevPhoto.src}
-          alt={prevPhoto.description}
-        />
-      )}
-      <img className={style.mainPhotoImage} src={activePhoto.src} alt={activePhoto.description} />
-      {nextPhoto && (
-        <img
-          className={style.mainPhotoImageNext}
-          src={nextPhoto.src}
-          alt={nextPhoto.description}
-        />
-      )}
-    </div>
+type RefT = React.MutableRefObject<HTMLDivElement | null>;
+
+const getPhotoByRef = (ref: RefT, index: number): HTMLElement | null =>
+  ref.current!.querySelector(`img:nth-of-type(${index + 1})`);
+
+const hidePhoto = (element: HTMLElement | null) => {
+  if (!element) return;
+
+  element.dataset.active = 'false';
+
+  if (element.previousSibling) {
+    // @ts-ignore
+    element.previousSibling.dataset.active = 'false';
+  }
+
+  if (element.nextSibling) {
+    // @ts-ignore
+    element.nextSibling.dataset.active = 'false';
+  }
+};
+
+const showPhoto = (element: HTMLElement | null) => {
+  if (!element) return;
+
+  element.dataset.active = 'true';
+  // @ts-ignore
+  element.src = element.dataset.src;
+
+  if (element.previousSibling) {
+    // @ts-ignore
+    element.previousSibling.dataset.active = 'prepared';
+    // @ts-ignore
+    element.previousSibling.src = element.previousSibling.dataset.src;
+  }
+
+  if (element.nextSibling) {
+    // @ts-ignore
+    element.nextSibling.dataset.active = 'prepared';
+    // @ts-ignore
+    element.nextSibling.src = element.nextSibling.dataset.src;
+  }
+};
+
+const MainPhoto: React.FC<IMainPhoto> = ({ photos, indexActivePhoto, className }) => {
+  const [prevActiveIndexPhoto, setPrevActiveIndexPhoto] = useState(indexActivePhoto);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  //
+  useLayoutEffect(() => {
+    if (!containerRef.current) return;
+
+    const activePhoto = getPhotoByRef(containerRef, prevActiveIndexPhoto);
+    const nextActivePhoto = getPhotoByRef(containerRef, indexActivePhoto);
+
+    if (prevActiveIndexPhoto !== indexActivePhoto) {
+      hidePhoto(activePhoto);
+      showPhoto(nextActivePhoto);
+    } else {
+      showPhoto(activePhoto);
+    }
+
+    setPrevActiveIndexPhoto(indexActivePhoto);
+  }, [indexActivePhoto]);
+  //
+  return useMemo(
+    () => (
+      <div className={cn(style.mainPhoto, className)} ref={containerRef}>
+        {photos.map((photo, id) => (
+          <img
+            className={style.mainPhotoImage}
+            key={photo.id}
+            data-active={id === indexActivePhoto}
+            data-src={photo.src}
+            alt={photo.description}
+          />
+        ))}
+      </div>
+    ),
+    [],
   );
 };
 
